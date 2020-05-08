6f42b4bb60c2f15c131dd6b0eb20b2439791a0bb
Nasra  Aden <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2020-05-08 10:56:06 -0700

    sprint so far

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,18 @@
 def get_indices_of_item_weights(weights, length, limit):
-
     """
     YOUR CODE HERE
     """
 
+    my_dict = {}
+    for i in range(length):
+        my_dict[weights[i]] = i
+    if limit - my_dict[weights[i]] in my_dict:
+        return 'logic here'
+    else:
+        return 'logic here'
     return None
+
+
+weights = [4, 6, 10, 15, 16]
+
+get_indices_of_item_weights(weights, 5, 21)
--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,9 @@
 def intersection(arrays):
-
     """
     YOUR CODE HERE
     """
+    my_dict = {}
+    result = []
 
     return result
 
@@ -10,8 +11,8 @@ def intersection(arrays):
 if __name__ == "__main__":
     arrays = []
 
-    arrays.append(list(range(1000000,2000000)) + [1,2,3])
-    arrays.append(list(range(2000000,3000000)) + [1,2,3])
-    arrays.append(list(range(3000000,4000000)) + [1,2,3])
+    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
